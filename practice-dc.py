665990aa47a2da5ed4fc70f97b870b0cce373f8c
Adam Agar <adamagar88@gmail.com>
2024-02-25 16:12:11 -0500

    Practice Data Containers

---
+++ practice-dc.py
@@ -0,0 +1,22 @@
+greeting = "Hello, World!"
+print(greeting)
+
+my_integer = 3
+print(my_integer)
+
+my_float = 0.2
+print(my_float)
+
+my_string = "Test"
+print(my_string)
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+
+fruits.append("orange")
+print(fruits)
+
+my_age = 33
+
+my_info = ["Adam", my_age]
+print(my_info)
