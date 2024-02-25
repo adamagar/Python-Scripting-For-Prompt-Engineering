d99a70443cd81525431de2957f9e330be868c378
Adam Agar <adamagar88@gmail.com>
2024-02-25 16:06:23 -0500

    Imlement Data Containers

---
+++ dataContainers.py
@@ -0,0 +1,11 @@
+my_name = "Adam"
+my_age = 33
+my_salary = 6.5
+is_hot = False
+my_fruits = ["apple", "banana", "strawberry"]
+
+print(my_fruits)
+
+my_fruits.append("mango")
+print(my_fruits)
+print(len(my_fruits))
