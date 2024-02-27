a032a6b3fb399f3b94d5df351a319232683d6d57
Adam Agar <adamagar88@gmail.com>
2024-02-27 13:35:07 -0500

    Implement modules in Python

--- functions.py
+++ functions.py
@@ -1,11 +1,10 @@
+import my_module
+import math
+
 def say_hello():
 	print("hello")
 
 say_hello()
 
-def print_list_elements(my_list):
-	for element in my_list:
-		print(f"I love: {element}")
-
 fruits = ["apple", "banana", "cherry"]
-print_list_elements(fruits)
+my_module.print_list_elements(fruits)
---
+++ my_module.py
@@ -0,0 +1,3 @@
+def print_list_elements(my_list):
+	for element in my_list:
+		print(f"I love: {element}")
