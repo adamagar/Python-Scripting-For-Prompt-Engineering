98ba967b69e26000c732650c84cedebae1e6c5a4
Adam Agar <adamagar88@gmail.com>
2024-02-27 12:24:06 -0500

    Practice operations

---
+++ practice-operations.py
@@ -0,0 +1,22 @@
+sum = 89 + 23
+sub = 130 - 57
+multipl = 7 * 8
+divide = 100 / 4
+rem = 19 % 5
+pw = 4 ** 3
+floor_div = 37 // 6
+
+print(sum)
+print(sub)
+print(multipl)
+print(divide)
+print(rem)
+print(pw)
+print(floor_div)
+
+lan = "Python"
+adj = "Rocks"
+print(lan + " " + adj)
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+print(list1 + list2)
