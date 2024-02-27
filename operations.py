747da6ad262be20cdeeb9474e5090abcdb68b915
Adam Agar <adamagar88@gmail.com>
2024-02-27 12:16:17 -0500

    Operations in Python

---
+++ operations.py
@@ -0,0 +1,9 @@
+first_number = 5
+second_number = 6
+sum = first_number + second_number
+result = first_number - second_number
+multipl = first_number * second_number
+
+print(sum)
+print(result)
+print(multipl)
