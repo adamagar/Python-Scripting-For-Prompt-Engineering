4c423f6a7955fb28c1cd6439286aaaff56a6f80c
Adam Agar <adamagar88@gmail.com>
2024-02-27 13:21:36 -0500

    Functios in Python

---
+++ functions.py
@@ -0,0 +1,11 @@
+def say_hello():
+	print("hello")
+
+say_hello()
+
+def print_list_elements(my_list):
+	for element in my_list:
+		print(f"I love: {element}")
+
+fruits = ["apple", "banana", "cherry"]
+print_list_elements(fruits)
