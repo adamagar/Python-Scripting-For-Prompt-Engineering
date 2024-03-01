9f3e660c86ffa03e6ba9e6b7ab0dd0b795cbc720
Adam Agar <adamagar88@gmail.com>
2024-03-01 15:15:54 -0500

    Update the code using prompts library

--- chatgpt.py
+++ chatgpt.py
@@ -1,5 +1,6 @@
 import helpers
 import llm
+import prompts
 
 token_count = 1000
 
@@ -7,11 +8,13 @@ costs = helpers.estimate_input_cost("gpt-3.5-turbo-0613", token_count)
 
 print(f"Costs: {costs}")
 
-prompt = "Generate 1 catchy youtube titles about [email marketing tricks]"
+prompt = prompts.twitter_thread_generator_prompt.format(topic="prompt engineering tips")
 
-num_tokens = helpers.count_tokens(prompt, "gpt-3.5-turbo")
-print(f"Token count: {num_tokens}")
+print(prompt)
 
 response = llm.llm_generate_text(prompt, "OpenAI", "gpt-3.5-turbo")
 
 print(response)
+
+num_tokens = helpers.count_tokens(prompt, "gpt-3.5-turbo")
+print(f"Token count: {num_tokens}")
