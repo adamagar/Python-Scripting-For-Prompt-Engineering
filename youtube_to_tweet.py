7d1b4c1e05f881d9fe3bbbf69fa3510f5daf807a
Adam Agar <adamagar88@gmail.com>
2024-03-03 22:41:31 -0500

    Implement tool summarize youtube videos, convert to tweets

---
+++ youtube_to_tweet.py
@@ -0,0 +1,15 @@
+import helpers
+import llm
+from prompt_templates import youtube_prompts
+
+selected_model = "gpt-3.5-turbo"
+
+youtube_url = "https://www.youtube.com/watch?v=tXQD9VKVmVM"
+
+video_transcript = helpers.get_video_transcript(youtube_url, language="tr")
+
+prompt = youtube_prompts.tweet_from_youtube_prompt.format(transcript=video_transcript)
+
+response = llm.llm_generate_text(prompt, "OpenAI", selected_model)
+
+print(response)
